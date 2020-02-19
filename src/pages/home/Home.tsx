589333d2f26c6a71b2772918f907a3acc64596b0
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2020-02-18 23:22:34 -0800

    css modules

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,18 @@
 import * as React from "react"
-
-import "./Home.scss"
+import styles from "./Home.module.scss"
 
 interface HomeProps {
     className?: string
 }
 
 export function Home({ className }: HomeProps): JSX.Element {
-    return <div className={`Home ${className ?? ""}`}>Home</div>
+    return (
+        <div className={`Home ${styles.Home} ${className ?? ""}`}>
+            Home
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    )
 }
