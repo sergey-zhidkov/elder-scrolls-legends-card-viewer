fa93ab059c74dd37e2be6d0dca6080af09bd410f
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2020-02-22 22:40:08 -0800

    Updates

---
+++ precache-manifest.e91a82324930da038b750980c3a83c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1ff44b33ef42652359d5b2578b1fc5",
+    "url": "/elder-scrolls-legends-card-viewer/index.html"
+  },
+  {
+    "revision": "b9d2d6dea5c869dfef2f",
+    "url": "/elder-scrolls-legends-card-viewer/static/css/main.82c734e0.chunk.css"
+  },
+  {
+    "revision": "d91eec9b38f0e083446e",
+    "url": "/elder-scrolls-legends-card-viewer/static/js/2.e76046ce.chunk.js"
+  },
+  {
+    "revision": "24e4538011227ed5892b76be6a7e3cd7",
+    "url": "/elder-scrolls-legends-card-viewer/static/js/2.e76046ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d2d6dea5c869dfef2f",
+    "url": "/elder-scrolls-legends-card-viewer/static/js/main.a67f9a82.chunk.js"
+  },
+  {
+    "revision": "fdbd8eaaf7c6a46c80ad",
+    "url": "/elder-scrolls-legends-card-viewer/static/js/runtime-main.042b3003.js"
+  }
+]);
